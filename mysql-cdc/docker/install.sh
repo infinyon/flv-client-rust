6ed7fadc577811ca5fb67c42d87e28459ee36915
ajhunyady <aj@fluvio.io>
2020-06-15 14:56:08 -0700

    updated install script to match README

--- mysql-cdc/docker/install.sh
+++ mysql-cdc/docker/install.sh
@@ -3,30 +3,29 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -n name -p port"
+   echo "Usage: $0 -n name -d data-path -p port"
    echo -e "\t-n Container name"
+   echo -e "\t-d Data path"
    echo -e "\t-p Port number"
    exit 1 # Exit script after printing help
 }
 
-while getopts "n:p:" opt
+while getopts "n:d:p:" opt
 do
    case "$opt" in
       n ) name="$OPTARG" ;;
+      d ) path="$OPTARG" ;;
       p ) port="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$name" ] || [ -z "$port" ]
+if [ -z "$name" ] || [-z "$path"] || [ -z "$port" ]
 then
    echo "Invalid parameters";
    helpFunction
 fi
-
-# Create path variable
-path=$HOME/mysql-cdc/$name
 
 # Create directory
 mkdir -p $path
