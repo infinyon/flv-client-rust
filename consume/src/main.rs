3c334dce1ce74c19dbfe4cad17c5937bc12fe67d
ajhunyady <aj@fluvio.io>
2020-07-23 14:18:21 -0700

    updated consumer main

--- consume/src/main.rs
+++ consume/src/main.rs
@@ -1,3 +1,35 @@
+//! Consume Crate
+//!
+//! # Consumer Example
+//!
+//! 1. Consumer connects to a fluvio cluster
+//! 2. Reads data streams with the following parameters
+//!     * topic:      **my-topic-1**
+//!     * partition:  **0**
+//!     * offset:     **FetchOffset::Earliest** (without an offset)
+//!
+//! ```
+//! let config = ScConfig::new(None, None).expect("connect");
+//! let mut client = config.connect().await.expect("should connect");
+//!
+//! let mut replica = client
+//!     .find_replica_for_topic_partition("my-topic-1", 0)
+//!     .await
+//!     .expect("replica missing");
+//! let mut log_stream = replica.fetch_logs(FetchOffset::Earliest(None), FetchLogOption::default());
+//!
+//! while let Some(response) = log_stream.next().await {
+//!     let records = response.records;
+//!     for batch in records.batches {
+//!         for record in batch.records {
+//!             if let Some(bytes) = record.value().inner_value() {
+//!                 let msg = String::from_utf8(bytes).expect("string");
+//!                 println!("{}", msg);
+//!             }
+//!         }
+//!     }
+//! }
+//! ```
 use futures::stream::StreamExt;
 
 use flv_client::profile::ScConfig;
@@ -12,6 +44,7 @@ fn main() {
     run_block_on(consume()).expect("run");
 }
 
+/// Adds one to the number given.
 async fn consume() -> Result<(), ClientError> {
     let config = ScConfig::new(None, None).expect("connect");
     let mut client = config.connect().await.expect("should connect");
